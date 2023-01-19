813cd71635e940f0a49feddea85c7be616b65d0c
Neo <lordneobarnachea@gmail.com>
2023-01-19 11:18:03 +0800

    added header and nav bar

--- bms/src/App.js
+++ bms/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        Barangay Management System
       </header>
+      <nav>
+          <a href="#">Home</a>
+          <a href="#">Services</a>
+          <a href="#">About</a>
+        </nav>
+      
     </div>
   );
 }
