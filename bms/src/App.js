7288287815e9bc33aa9a26f09bbc817f2d99727f
Neo <lordneobarnachea@gmail.com>
2023-01-22 20:59:45 +0800

    added navbar

--- bms/src/App.js
+++ bms/src/App.js
@@ -1,19 +1,22 @@
-import './App.css';
+import Navbar from "./Navbar"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import { Route, Routes } from "react-router-dom"
+import Services from "./pages/Services"
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        Barangay Management System
-      </header>
-      <nav>
-          <a href="#">Home</a>
-          <a href="#">Services</a>
-          <a href="#">About</a>
-        </nav>
-      
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
