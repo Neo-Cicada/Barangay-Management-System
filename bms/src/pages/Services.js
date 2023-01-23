a0e5212d3a3e889cdfc26c9c1eb6863c3ad2dd93
Neo <lordneobarnachea@gmail.com>
2023-01-23 10:40:44 +0800

    added form template

--- bms/src/pages/Services.js
+++ bms/src/pages/Services.js
@@ -1,3 +1,41 @@
+import React, { useState } from 'react';
+
 export default function Services() {
-    return <h1>Services</h1>
+  const [lastname, setLastname] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form submitted with lastname:', lastname, 'firstname:', firstname, 'phone:', phone, 'email:', email);
   }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label for="Lastname">LastName</label>
+        <input type="name" className="Lastname" id="Lastname" value={lastname} onChange={(event) => setLastname(event.target.value)}>
+        </input>
+      </div>
+      <div>
+        <label for="Lastname">FirstName</label>
+        <input type="name" className="Firstname" id="Firstname" value={firstname} onChange={(event) => setFirstname(event.target.value)}>
+        </input>
+      </div>
+      <div>
+        <label for="phone-no">Phone Number</label>
+        <input type="tel" id="phone-no" className="phone-no" value={phone} onChange={(event) => setPhone(event.target.value)}>
+        </input>
+      </div>
+      <div>
+        <label for="phone-no">Email</label>
+        <input type="email" id="email" className="email" value={email} onChange={(event) => setEmail(event.target.value)}>
+        </input>
+      </div>
+      
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
