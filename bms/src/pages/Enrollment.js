4b9c84060fc97ad1c10345f0fdbb99606cde41d6
Neo <lordneobarnachea@gmail.com>
2023-01-26 14:40:58 +0800

    added components

---
+++ bms/src/pages/Enrollment.js
@@ -0,0 +1,6 @@
+import React from "react"
+export default function Enrollment(){
+    return( <h1>
+        Enrollment
+    </h1>)
+}
---
+++ bms/src/pages/Facilities.js
@@ -0,0 +1,5 @@
+export default function Facilities(){
+    return(
+        <h1>Facilities</h1>
+    )
+}
