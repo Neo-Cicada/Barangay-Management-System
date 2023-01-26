793641bb347f42deee792e55bcf0c7c67c4e70b5
Neo <lordneobarnachea@gmail.com>
2023-01-26 15:10:13 +0800

    fixed something

--- bms/src/pages/Form.js
+++ bms/src/pages/Form.js
@@ -15,17 +15,17 @@ export default function Form() {
   return (
     <form onSubmit={handleSubmit}  className={style.form}>
       <div>
-        <label for="Lastname" className={style.label}>LastName</label>
+        <label for="Lastname" className={style.label}>Lastname</label>
         <input type="name" className={style.input} id="Lastname" value={lastname} onChange={(event) => setLastname(event.target.value)}>
         </input>
       </div>
       <div>
-        <label for="Lastname" className={style.label} >FirstName</label>
+        <label for="Lastname" className={style.label} >Firstname</label>
         <input type="name" className={style.input} id="Firstname" value={firstname} onChange={(event) => setFirstname(event.target.value)}>
         </input>
       </div>
       <div>
-        <label for="phone-no" className={style.label}>Phone Number</label>
+        <label for="phone-no" className={style.label}>Phone number</label>
         <input type="tel" id="phone-no" className={style.input} value={phone} onChange={(event) => setPhone(event.target.value)}>
         </input>
       </div>
